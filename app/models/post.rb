b01e8af58fb1726dd5ef2dc07ca689202081389a
Vishal Sharma <vsh1819@gmail.com>
2018-07-16 18:19:40 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,17 +7,17 @@ class Post < ActiveRecord::Base
          validate :is_clickbait?
 
 
-CLICKBAIT_PATTERNS = [
-    /Won't Believe/i,
-    /Secret/i,
-    /Top [0-9]*/i,
-    /Guess/i
-  ]
+# CLICKBAIT_PATTERNS = [
+#     /Won't Believe/i,
+#     /Secret/i,
+#     /Top [0-9]*/i,
+#     /Guess/i
+#   ]
 
-  def is_clickbait?
-    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
-      errors.add(:title, "must be clickbait")
-    end
-  end
+#   def is_clickbait?
+#     if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+#       errors.add(:title, "must be clickbait")
+#     end
+#   end
 
 end
